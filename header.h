57a9b97aea64dd17aa0b4677df5c86adfc13bce3
Francisco Ferreira <forofrancisco4@gmail.com>
2024-10-29 13:55:55 +0000

    Ajusta header.h para remover duplicatas e organizar funções

--- header.h
+++ header.h
@@ -1,36 +1,27 @@
-// header.h
 #ifndef HEADER_H
 #define HEADER_H
-#include <string.h>
-#include <stdio.h>
-#include <stdbool.h>  
-#include <stdlib.h>
-#include <time.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <unistd.h>
 
+#include <stdbool.h>  // Inclui a definição de bool
+#include <stdio.h>    // Para printf, fprintf, etc.
+#include <stdlib.h>   // Para malloc, free, etc.
+#include <string.h>   // Para manipulação de strings (strcat, strcmp, etc.)
+#include <unistd.h>   // Para close (encerrar sockets)
+#include <arpa/inet.h> // Para definições de sockets (struct sockaddr_in, htons, etc.)
+#include <sys/types.h>  // Para tipos de dados de sockets
+#include <sys/socket.h> // Para funções de sockets
+#include <errno.h>      // Para tratamento de erros
+#include <time.h>       // Para manipulação de tempo
 
 #define TAMANHO 9
-// Sockets --------------------------------------------------------------------------------
-
-
-#define SOCKET_NAME "/tmp/DemoSocket"
-#define BUFFER_SIZE 128
-//---------------------------------------------------------------------
 
 // Funções relacionadas ao jogo de Sudoku
-void jogar_sudoku(int tabuleiro[TAMANHO][TAMANHO]);
-void gerar_sudoku(int tabuleiro[TAMANHO][TAMANHO], int dificuldade);
-int resolver_sudoku(int tabuleiro[TAMANHO][TAMANHO], int linha, int col);
-void imprimir_tabuleiro(int tabuleiro[TAMANHO][TAMANHO]);
-int verificar_vitoria(int tabuleiro[TAMANHO][TAMANHO]);
-int pode_colocar(int tabuleiro[TAMANHO][TAMANHO], int linha, int col, int num);
-void gravar_solucao(int tabuleiro[TAMANHO][TAMANHO], const char *nome_ficheiro_solucoes, int idSudoku);
-void escrever_log(const char *mensagem);
-
-bool resolver(int tabuleiro[TAMANHO][TAMANHO]);
-void imprimir_tabuleiro_cliente(int tabuleiro[TAMANHO][TAMANHO]);
+void jogar_sudoku(int tabuleiro[TAMANHO][TAMANHO], int sock);
 void escrever_log_cliente(const char *mensagem);
-//void connect_server();
-#endif
+bool verificar_vitoria(int tabuleiro[TAMANHO][TAMANHO]);
+bool isValid(int tabuleiro[TAMANHO][TAMANHO], int linha, int col, int num);
+bool findEmptyCell(int tabuleiro[TAMANHO][TAMANHO], int *linha, int *col);
+bool pode_colocar(int tabuleiro[TAMANHO][TAMANHO], int linha, int col, int num);
+char* formatar_tabuleiro(int tabuleiro[TAMANHO][TAMANHO]);
+void imprimir_tabuleiro_cliente(int tabuleiro[TAMANHO][TAMANHO]);
+
+#endif // HEADER_H
